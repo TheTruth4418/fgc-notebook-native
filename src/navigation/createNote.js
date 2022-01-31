00a4f894fe424661ee4a9ff9e2d1419e23e72cba
William West <wwest466@gmail.com>
2022-01-31 16:57:01 -0600

    Drop Down forms per game available

--- src/navigation/createNote.js
+++ src/navigation/createNote.js
@@ -1,13 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux';
+import { Picker } from 'react-native-web';
 
 function CreateNote(props){
     let gameId = props.route.params.gameId
     let game = Object.keys(props.games)[gameId]
+    const characters = Object.keys(props.games[game])
+    const [selectedCharacter, setSelectedCharacter] = useState();
+
+    const charsForm = () => {
+        let arr = [];
+        characters.forEach(char => {
+            arr.push(<Picker.Item label={char} key={char} value = {char}/>)
+        })
+        return(arr)
+    }
+
     return (
         <View>
             <Text>{`Create mode for ${game}`}</Text>
+            <Picker
+                    selectedValue={selectedCharacter}
+                    onValueChange={(char, charIndex) =>
+                        setSelectedCharacter(char)
+                }>
+                {charsForm()}
+            </Picker>
         </View>
     )
 }
--- src/navigation/viewNotes.js
+++ src/navigation/viewNotes.js
@@ -1,13 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
-import React from 'react';
+import React, {useState} from 'react';
+import { Picker } from 'react-native-web';
 
 function ViewNotes(props){
     let gameId = props.route.params.gameId
     let game = Object.keys(props.games)[gameId]
+    const characters = Object.keys(props.games[game])
+
+    const [selectedCharacter, setSelectedCharacter] = useState();
+
+    const charsForm = () => {
+        let arr = [];
+        characters.forEach(char => {
+            arr.push(<Picker.Item label={char} key={char} value = {char}/>)
+        })
+        return(arr)
+    }
+
     return (
         <View>
             <Text>{`View mode for ${game}`}</Text>
+            <Picker
+                    selectedValue={selectedCharacter}
+                    onValueChange={(char, charIndex) =>
+                        setSelectedCharacter(char)
+                }>
+                {charsForm()}
+            </Picker>
         </View>
     )
 }
